dbe98bb2637872f19b48d6bb2d54a3f4a2d1d026
vickel <vickelleung@gmail.com>
2022-03-25 17:24:22 -0400

    Added dependencies, components, pages and assets

---
+++ src/Page/WorkPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+function WorkPage() {
+  return (
+    <PageWrapper>
+      <Title>WORK</Title>
+    </PageWrapper>
+  );
+}
+
+export default WorkPage;
+
+const PageWrapper = styled.div`
+  margin: 2%;
+`;
+
+const Title = styled.div`
+  font-size: 2em;
+`;
